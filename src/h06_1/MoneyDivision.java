778ffbb1a56610ef537df0dedc631a8c47e0290d
xPandaNiji <anon@git.hub>
2015-09-15 14:31:08 +0200

    code cleanup

--- src/h06_1/MoneyDivision.java
+++ src/h06_1/MoneyDivision.java
@@ -4,18 +4,16 @@ import java.applet.Applet;
 import java.awt.Graphics;
 
 public class MoneyDivision extends Applet{
-
-	double cash = (double) 113 / 4;
 	
 	public void init() {
 		
 	}
 	
 	public void paint(Graphics g) {
-		g.drawString("Jan krijgt €" + cash, 20, 20);
-		g.drawString("Ali krijgt €" + cash, 20, 35);
-		g.drawString("Jeanette krijgt €" + cash, 20, 50);
-		g.drawString("Daniel krijgt €" + cash, 20, 65);
+		g.drawString("Jan krijgt €" + (double) 113/4, 20, 20);
+		g.drawString("Ali krijgt €" + (double) 113/4, 20, 35);
+		g.drawString("Jeanette krijgt €" + (double) 113/4, 20, 50);
+		g.drawString("Daniel krijgt €" + (double) 113/4, 20, 65);
 	}
 	
 }
